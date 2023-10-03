122929114a11679ea1be80f0e5c9a0d45146e81b
GLimael <gabriel23052006@gmail.com>
2023-10-02 21:51:23 -0300

    Primeira aula de react

---
+++ React-Vite/react-vite/src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+const Banner = () =>{
+    return(
+        <header className='banner'>
+            <img src='./images/fundo.jpg' alt='Banner do site' />
+        </header>
+    )
+}
+
+export default Banner;
---
+++ React-Vite/react-vite/src/components/Formulario/index.jsx
@@ -0,0 +1,16 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto';
+
+const Formulario = () => {
+    return (
+        <section className='formulario'>
+            <form>
+                <h2>Preencha seus dados para roubarmos eles: </h2>
+                <CampoTexto label='Nome' placeholder='Digite o seu Nome' />
+                
+            </form>
+        </section>
+    );
+};
+
+export default Formulario;
